c62c4250a77cd31380254dde0f4fc06c451f4cad
Dominique Skaf <doskaf@yahoo.com>
2019-10-15 20:18:38 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,12 @@
 INSERT INTO projects VALUES ('project_1', 'category', 800, 101519, 121419);
+INSERT INTO projects VALUES ('project_2', 'category', 800, 101519, 121419);
+INSERT INTO projects VALUES ('project_3', 'category', 800, 101519, 121419);
+INSERT INTO projects VALUES ('project_4', 'category', 800, 101519, 121419);
+INSERT INTO projects VALUES ('project_5', 'category', 800, 101519, 121419);
+INSERT INTO projects VALUES ('project_6', 'category', 800, 101519, 121419);
+INSERT INTO projects VALUES ('project_7', 'category', 800, 101519, 121419);
+INSERT INTO projects VALUES ('project_8', 'category', 800, 101519, 121419);
+INSERT INTO projects VALUES ('project_9', 'category', 800, 101519, 121419);
+INSERT INTO projects VALUES ('project_10', 'category', 800, 101519, 121419);
+
+INSERT INTO users VALUES ('henry', 27);
